c0c72e09c1190687d6ef98611ef40d26a58cafcc
LukeLin <773000785@qq.com>
2016-09-12 11:38:01 +0800

    add server build

---
+++ webpack.config-server.js
@@ -0,0 +1,65 @@
+let fs = require('fs');
+let path = require('path');
+let webpack = require('webpack');
+let packageJson = require('./package.json');
+
+
+module.exports = {
+    entry: path.resolve(__dirname, './server/app.js'),
+
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: 'server.js',
+        libraryTarget: 'commonjs2'
+    },
+
+    devtools: 'cheap-module-source-map',
+
+    target: 'node',
+
+    node: {
+        __filename: true,
+        __dirname: true
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        modulesDirectories: [
+            // 'client',
+            'node_modules',
+        ],
+    },
+
+    externals: Object.keys(packageJson.dependencies),
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules|jce/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        'react'
+                    ],
+                    plugins: [
+                        "transform-es2015-modules-commonjs",
+                        "transform-async-to-generator",
+                        "transform-runtime"
+                    ]
+                },
+            }, {
+                test: /\.json$/,
+                loader: 'json-loader',
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({"process.env": {NODE_ENV: '"production"'}}),
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        // new webpack.BannerPlugin('require("source-map-support").install();',
+        //     { raw: true, entryOnly: false }),
+        new webpack.IgnorePlugin(/(\.(css|less)$)|zepto/)
+    ],
+};
