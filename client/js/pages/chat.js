745c0ccb1750a15cc2bf84b7918a41d1fb8d5204
lukelin <773000785@qq.com>
2016-10-20 01:01:25 +0800

    update

--- client/js/pages/chat.js
+++ client/js/pages/chat.js
@@ -11,16 +11,4 @@ let createApp = createRender();
 createApp({
     null,
     component: <Page/>
-}).then((store) => {
-    if (process.env.NODE_ENV !== 'production' && module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept(['../../../common/pages/chat/Page.jsx', '../../../common/pages/chat/reducers'], () => {
-            const NewPage = require('../../../common/pages/chat/Page.jsx').default;
-            const newRootReducer = require('../../../common/pages/chat/reducers').default;
-            createApp({
-                rootReducer: newRootReducer,
-                component: <NewPage/>
-            });
-        });
-    }
 });
