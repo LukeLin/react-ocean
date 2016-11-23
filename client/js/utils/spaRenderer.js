035cc67733fc7a85c6851dfb20e7bd6b6bf1619a
lukelin <773000785@qq.com>
2016-11-23 22:15:33 +0800

    update

--- client/js/utils/spaRenderer.js
+++ client/js/utils/spaRenderer.js
@@ -10,15 +10,15 @@ import App from '../../../common/App';
 const store = configureStore(window.__INITIAL_STATE__, browserHistory);
 const routes = createRoutes(store);
 
-// function onUpdate(){
-//     window.scrollTo(0, 0);
-// }
+function onUpdate(){
+    window.scrollTo(0, 0);
+}
 
 match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
     render(
         <Provider store={store}>
             <App appConfig={ window.__APP_CONFIG__ }>
-                <Router history={browserHistory} { ...renderProps } onUpdate={ null }/>
+                <Router history={browserHistory} { ...renderProps } onUpdate={ onUpdate }/>
             </App>
         </Provider>, document.getElementById('page')
     );
