0e7eafc31d5f4ade1a2d03dfb5de724e78b2bc95
LukeLin <773000785@qq.com>
2016-12-02 15:54:34 +0800

    update sw.js

--- client/js/utils/sw.js
+++ client/js/utils/sw.js
@@ -1,107 +1,99 @@
-'use strict';
+var config = {
+    db: 'sw_gamPportal_201612011014'
+};
 
-/**
- * http://www.html5rocks.com/en/tutorials/service-worker/introduction/
- */
-
-let CACHE_NAME = 'react-ocean-v1';
-let urlsToCache = [
-    /chat-\w+?.js/,
-    '/static/css/main-min.css',
-    '/static/js/libs-min.js',
-    '/static/js/min/index.js'
+var urlsToCache = [
+    
 ];
 
-self.addEventListener('install', function (event) {
-    console.info('---on service worker install---');
-    // Perform install steps
-    // event.waitUntil(
-    //     caches.open(CACHE_NAME)
-    //         .then(function (cache) {
-    //             console.log('Opened cache');
-    //             return cache.addAll(urlsToCache);
-    //         })
-    // );
+var addToCache = function (req) {
+    return fetch(req.clone()).then(function (resp) {
+        var cacheResp = resp.clone();
+        if (resp.status !== 200 || (resp.type !== 'basic' && resp.type !== 'cors')) {
+            return resp;
+        }
+        caches.open(config.db).then(function (cache) {
+            cache.put(req.clone(), cacheResp);
+        });
+        return resp;
+    });
+};
+
+
+// self.addEventListener('install', function (e) {
+//     // See https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-global-scope-skipwaiting
+//     if (typeof self.skipWaiting === 'function') {
+//         e.waitUntil(self.skipWaiting());
+//     }
+
+// });
+
+self.addEventListener('activate', function (event) {
+    // event.waitUntil(Promise.all([
+    //     caches.keys().then(function (cacheNames) {
+    //         return Promise.all(cacheNames.map(function (cacheName) {
+    //             if (cacheName !== config.db) {
+    //                 return caches.delete(cacheName);
+    //             }
+    //         }));
+    //     }),
+    //     typeof self.clients.claim === 'function' && self.clients.claim()
+    // ]));
+
+    // 内核的cache.delete有bug，这里先注释
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.map(function (cacheName) {
+            if (cacheName !== config.db) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
 });
 
-self.addEventListener('fetch', function(event) {
-    let match = false;
-    for(let url of urlsToCache){
-        if(url instanceof RegExp){
-            match = url.test(event.request.url);
-        } else if(event.request.url.indexOf(url) >= 0){
+self.addEventListener('fetch', function (event) {
+    var requestUrl = event.request.url;
+
+    var match = false;
+    for (var i = 0; i < urlsToCache.length; ++i) {
+        var url = urlsToCache[i];
+        if (url instanceof RegExp) {
+            match = url.test(requestUrl);
+        } else if (requestUrl.indexOf(url) >= 0) {
             match = true;
             break;
         }
     }
 
-    if(!match) {
-        // event.respondWith(fetch(event.request));
+    if (!match) {
+        // event.respondWith(fetch(event.request.clone()));
         return;
     }
 
-    console.log(`service worker fetching ${ event.request.url }`);
+    var promise, req, url = event.request.url;
 
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                console.log('response: ' + response);
-                // Cache hit - return response
-                if (response) {
-                    console.log(`${ event.request.url } cache hit`);
-                    return response;
-                }
+    if (url.indexOf('/ajax/') !== -1 || url.indexOf('bypass=1') !== -1 || url.indexOf('http:') === 0) {
+        event.respondWith(fetch(event.request.clone()));
+        return;
+    }
 
-                // IMPORTANT: Clone the request. A request is a stream and
-                // can only be consumed once. Since we are consuming this
-                // once by cache and once by the browser for fetch, we need
-                // to clone the response
-                let fetchRequest = event.request.clone();
+    // if (url.indexOf('cors=1') !== -1) {
+        req = new Request(url, {
+            mode: 'cors'
+        });
+    // } else {
+        // req = event.request.clone();
+    // }
 
-                return fetch(fetchRequest, {
-                    mode: 'cors',
-                    credentials: 'omit'
-                }).then(
-                    function(response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || (response.type !== 'basic' && response.type !== 'cors')) {
-                            return response;
-                        }
+    promise = caches.open(config.db).then(function (cache) {
+        return cache.match(req);
+    }).then(function (response) {
+        if (response) {
+            return response;
+        } else {
+            return addToCache(req);
+        }
+    });
 
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have 2 stream.
-                        let responseToCache = response.clone();
+    event.respondWith(promise);
 
-                        caches.open(CACHE_NAME)
-                            .then(function(cache) {
-                                console.log(`${ event.request.url } put into cache`);
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    console.info('---on service worker activate---');
-
-    let cacheWhitelist = ['pages-cache-v1'];
-
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        console.log(`cache ${ cacheName } deleted`);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
 });
